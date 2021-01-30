ebc03f42f7c691b52f355c0f1bbc53c4f82b3ee9
yxcho <choyangxuan@me.com>
2021-01-30 16:08:33 +0800

    added post and get fb webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,13 @@
 import express from "express";
 import homepageController from "../controllers/homepageController.js";
+import chatbotController from "../controllers/chatbotController.js";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
   router.get("/", homepageController.getHomePage);
+  router.get("/webhook", chatbotController.getWebhook); // to verify the facebook webhook
+  router.post("/webhook", chatbotController.postWebhook); // to communicate with fb server
   return app.use("/", router);
 };
 
