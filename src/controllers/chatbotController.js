bd46d85001284651c9a91fa366a4da105c846b11
yxcho <choyangxuan@me.com>
2021-01-30 17:00:06 +0800

    updated webhook

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -5,22 +5,26 @@ let postWebhook = (req, res) => {
   // postwebhook receives every update from facebook
   // whenever a client sends a message to our facebook page,
   // fb will send signal to our node server telling the server to do something for the client
+  // Parse the request body from the POST
   let body = req.body;
 
-  // Checks this is an event from a page subscription
+  // Check the webhook event is from a Page subscription
   if (body.object === "page") {
-    // Iterates over each entry - there may be multiple if batched
+    // Iterate over each entry - there may be multiple if batched
     body.entry.forEach(function (entry) {
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id;
+      console.log("Sender PSID: " + sender_psid);
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send("EVENT_RECEIVED");
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 };
@@ -48,6 +52,14 @@ let getWebhook = (req, res) => {
   }
 };
 
+// Handles messages events
+function handleMessage(sender_psid, received_message) {}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
 module.exports = {
   postWebhook: postWebhook,
   getWebhook: getWebhook,
