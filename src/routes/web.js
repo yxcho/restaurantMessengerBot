371e32c504d41f0798f4d53b47cb26ff1dcab338
yxcho <choyangxuan@me.com>
2021-01-30 15:45:58 +0800

    deploy to heroku

---
+++ src/routes/web.js
@@ -0,0 +1,11 @@
+import express from "express";
+import homepageController from "../controllers/homepageController.js";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+  router.get("/", homepageController.getHomePage);
+  return app.use("/", router);
+};
+
+module.exports = initWebRoutes;
