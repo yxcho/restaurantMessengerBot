c46cf735bbcdd895990a8f988444c15b0476f5e1
yxcho <choyangxuan@me.com>
2021-01-31 19:17:32 +0800

    blind

--- src/services/chatBotServices.js
+++ src/services/chatBotServices.js
@@ -2,26 +2,26 @@ import request from "request";
 require("dotenv").config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
-
-let getFacebookUsername = (sender_psid) => {
-  return new Promise((resolve, reject) => {
+let getFacebookUsername = function (sender_psid) {
+  return new Promise(function (resolve, reject) {
     // Send the HTTP request to the Messenger Platform
-    let uri = `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`;
+    let url = `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`;
     request(
       {
-        uri: uri,
+        uri: url,
         method: "GET",
       },
 
-      function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          // Print out the response body
-          body = JSON.parse(body);
-          let username = `${body.last_name} ${body.first_name}`;
-          res.send(body);
+      function (err, res, body) {
+        if (!err) {
+          console.log(body);
+          resolve(JSON.parse(body));
+        //   body = JSON.parse(body)
+        //   let username = `${body.last_name}, ${body.first_name}`
+        //   resolve(username)
         } else {
-          // TODO: Handle errors
-          res.send(error);
+          console.error("Unable to send message:" + err);
+          reject(err);
         }
       }
     );
